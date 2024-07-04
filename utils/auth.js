aeb6ef6c336eb8049bfd8c40c62cfbd57b957424
Mitra USYD <mitraahmadi129@gmail.com>
2024-07-04 11:29:13 +1000

    fixing error in my code which causedbugs in applicaiton

--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,6 @@
 // creating a middleware function in utils to avoid repetitive code in the routes
 const withAuth = (req, res, next) => {
-  if (!req.session.loggedIn) {
+  if (!req.session.logged_in) {
     res.redirect("/login");
   } else {
     next();
